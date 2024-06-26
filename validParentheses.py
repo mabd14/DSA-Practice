ce4184d9efa6860c46fa83df0964c30a4cb0eb3e
Mahamed Abdulle <107773662+mabd14@users.noreply.github.com>
2024-06-27 00:10:46 +0100

    Update validParentheses.py
    added explanation

--- validParentheses.py
+++ validParentheses.py
@@ -1,13 +1,27 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        pending = [] # 0(1)
-        pair = {'{': '}', '[': ']', '(': ')'} # 0(1)
-        open_par = '({[' # 0(1)
-        for letter in s: # 0(N)
-            if letter in open_par: # 0(1)
-                pending.append(letter) # 0(1)
+        pending = [] 
+        pair = {'{': '}', '[': ']', '(': ')'} 
+        open_par = '({[' 
+        for letter in s: 
+            if letter in open_par: 
+                pending.append(letter) 
             else:
-                if len(pending) == 0 or pair[pending.pop()] != letter: # 0(1)
+                if len(pending) == 0 or pair[pending.pop()] != letter: 
                     return False
-        # 0(1)*  0(N)
+        
         return len(pending) == 0 
+
+"""
+    pending: a stack that stores opening parantheses and validate their matching closing parantheses in the correct order
+    pair: a dictionary that has the key as the open parantheses and the value as the closing parantheses
+    open_par: all opening parantheses
+
+    for letter in s: -> Iterate over each character in the input string
+        if letter in open_par:  -> Check if the character is an opening parenthesis
+            pending.append(letter)  -> If it is, push it onto the stack
+        else:
+            if len(pending) == 0 or pair[pending.pop()] != letter:  -> If the character is a closing parenthesis, check for mismatched or unbalanced parentheses
+                return False  -> Return False if there is a mismatch or no opening parenthesis to match
+        return len(pending) == 0  -> Return True if all parentheses are balanced (stack is empty), otherwise False
+"""
